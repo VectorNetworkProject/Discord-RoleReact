0d3749b1a614538cf539f745bafc553b5a0f200d
InkoHX <inkohx@gmail.com>
2018-10-13 01:52:22 +0900

    Update roleReact.js

--- roleReact.js
+++ roleReact.js
@@ -1,68 +1,82 @@
-//Settings!
-const yourID = ""; //Instructions on how to get this: https://redd.it/40zgse
-const setupCMD = "!createrolemessage"
-let initialMessage = `**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**`;
-const roles = ["Hacker", "Artist", "Public Relations", "Intern"];
-const reactions = ["💻", "🖌", "😃", "🆕"];
-const botToken = ""; /*You'll have to set this yourself; read more
-                     here https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token*/
+'use strict';
+require('dotenv').config();
 
-//Load up the bot...
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-bot.login(botToken);
+const { bot, roles, reactions } = require('./roles'),
+    yourID = process.env.USERID,
+    setupCMD = 'vnp!role';
+let initialMessage = '**React to the messages below to receive the associated role. If you would like to remove the role, simply remove your reaction!**\n**役職を貰うためには当てはまるものにリアクションを付けて下さい。役職を外す場合はリアクションを外せば外れます。**';
+bot.login(process.env.TOKEN);
 
-//If there isn't a reaction for every role, scold the user!
-if (roles.length !== reactions.length) throw "Roles list and reactions list are not the same length!";
+if (roles.length !== reactions.length) console.log('Roles list and reactions list are not the same length!');
 
-//Function to generate the role messages, based on your settings
 function generateMessages(){
-    var messages = [];
+
+    let messages = [];
     messages.push(initialMessage);
-    for (let role of roles) messages.push(`React below to get the **"${role}"** role!`); //DONT CHANGE THIS
+    for (let role of roles) messages.push(`React below to get the **"${role}"** role!\n**${role}**の役職を付ける場合は下のリアクションを追加して下さい。`);
     return messages;
+
 }
 
 
-bot.on("message", message => {
-    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
-        var toSend = generateMessages();
-        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+bot.on('message', (message) => {
+
+    if (message.author.id === yourID && message.content.toLowerCase() === setupCMD){
+
+        let toSend = generateMessages(),
+            mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
         for (let mapObj of mappedArray){
-            message.channel.send(mapObj[0]).then( sent => {
+
+            message.channel.send(mapObj[0]).then( (sent) => {
+
                 if (mapObj[1]){
-                  sent.react(mapObj[1]);  
-                } 
-            });
-        }
-    }
-})
 
+                    sent.react(mapObj[1]);
 
-bot.on('raw', event => {
-    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
-        
-        let channel = bot.channels.get(event.d.channel_id);
-        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
-        let user = msg.guild.members.get(event.d.user_id);
-        
-        if (msg.author.id == bot.user.id && msg.content != initialMessage){
-       
-            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
-            var role = msg.content.match(re)[1];
-        
-            if (user.id != bot.user.id){
-                var roleObj = msg.guild.roles.find(r => r.name === role);
-                var memberObj = msg.guild.members.get(user.id);
-                
-                if (event.t === "MESSAGE_REACTION_ADD"){
-                    memberObj.addRole(roleObj)
-                } else {
-                    memberObj.removeRole(roleObj);
                 }
-            }
+
+            });
+
         }
-        })
- 
-    }   
+
+    }
+
+});
+
+
+bot.on('raw', (event) => {
+
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t === 'MESSAGE_REACTION_REMOVE'){
+
+        let channel = bot.channels.get(event.d.channel_id),
+            message = channel.fetchMessage(event.d.message_id).then((msg)=> {
+
+                let user = msg.guild.members.get(event.d.user_id);
+                if (msg.author.id === bot.user.id && msg.content !== initialMessage){
+
+                    let re = '\\*\\*"(.+)?(?="\\*\\*)',
+                        role = msg.content.match(re)[1];
+                    if (user.id !== bot.user.id){
+
+                        let roleObj = msg.guild.roles.find((r) => r.name === role),
+                            memberObj = msg.guild.members.get(user.id);
+
+                        if (event.t === 'MESSAGE_REACTION_ADD'){
+
+                            memberObj.addRole(roleObj);
+
+                        } else {
+
+                            memberObj.removeRole(roleObj);
+
+                        }
+
+                    }
+
+                }
+
+            });
+
+    }
+
 });
