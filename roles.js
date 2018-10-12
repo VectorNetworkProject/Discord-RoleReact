2913069dfc77a7851a755308cc82fde9fd17dde7
InkoHX <inkohx@gmail.com>
2018-10-13 01:52:26 +0900

    Update roles.js

---
+++ roles.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const
+    Discord = require('discord.js'),
+    roles = ['NoPvP', 'PvP', 'English｜英語', 'Japanese｜日本語', 'Build｜建築', 'Programming | プログラミング', 'Gamer｜ゲーマー'], reactions = ['😂', '😎', '😁', '😀', '😶', '😬', '😶'],  bot = new Discord.Client();
+exports.roles = roles;
+exports.reactions = reactions;
+exports.Discord = Discord;
+exports.bot = bot;
